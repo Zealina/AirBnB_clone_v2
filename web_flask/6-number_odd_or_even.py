dd121d8c4a9e3ba174a388c5b68d7cb9dbb11905
Sebastine Hamilton <hamilsebastine@gmail.com>
2023-12-26 12:08:36 +0000

    Control Flow Odd or Even

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -46,5 +46,11 @@ def is_number_template(n):
     return render_template('5-number.html', n=n)
 
 
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def is_number_odd_or_even(n):
+    """Returns Whether a number is odd or even"""
+    return render_template('6-number_odd_or_even.html', n=n)
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
