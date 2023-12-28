de5802da0945d4a023dcc43224bd54f686d3872b
Sebastine Hamilton <hamilsebastine@gmail.com>
2023-12-28 12:47:29 +0000

    Generate HTML list

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Flask Web Application to display a list of states in storage"""
+from models import storage
+from flask import Flask, render_template
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def rm_current_session(exception=None):
+    """Remove the current session of SQLAlchemy if db"""
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    return render_template("7-states_list.html", objects=storage.all(State))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
